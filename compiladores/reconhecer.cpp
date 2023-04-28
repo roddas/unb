227841ce16fbf849699be1001b61c9d71a67382b
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-04-28 12:17:18 -0300

    feat: Add the digito function

--- compiladores/reconhecer.cpp
+++ compiladores/reconhecer.cpp
@@ -6,18 +6,35 @@ char lookahead;
 
 void reconhecer(char);
 char next_char(void);
+void erro(void);
+void digito(void);
 
 int main(void){
 	
+	lookahead = next_char();
+	//reconhecer(lookahead);
+	digito();
 	return 0;
 }
 
 void reconhecer(char token){
-	if(lookahead == token)
-		lookahead = next_char();
+	if(token == lookahead )
+		token = next_char();
 	else
 		erro();
 }
 char next_char(void){
 	return getchar();
+}
+void erro(void){
+	printf("Erro: Caractere inválido\n");
+	exit(-1);
+}
+void digito(void){
+	
+	if(!isdigit(lookahead))
+		erro();
+		
+	printf("%c",lookahead);
+	reconhecer(lookahead);
 }
