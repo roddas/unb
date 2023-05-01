f9d92d0c684eef2a74933e725e34419c07c45c18
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-05-01 16:19:27 -0300

    feat: Add the reconhecer procediment

--- compiladores/proximo_token.cpp
+++ compiladores/proximo_token.cpp
@@ -7,6 +7,7 @@ token lookahead;
 
 token proximo_token(void);
 void erro(void);
+void reconhecer(token);
 
 int main(void){
 	token t = proximo_token();
@@ -21,4 +22,10 @@ token proximo_token(void)
 void erro(void){
 	cerr << "\nErro de sintaxe! lookahead = " << lookahead << '\n';
 	exit(-1);
+}
+void reconhecer(token t){
+	if(lookahead == t)
+		lookahead = proximo_token();
+	else
+		erro();
 }
