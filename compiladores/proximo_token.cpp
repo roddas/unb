8eabe661ea8b41a97cfa4a018c692ab304d62cf5
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-05-01 16:22:31 -0300

    feat: Add the resto, digito and expr functions

--- compiladores/proximo_token.cpp
+++ compiladores/proximo_token.cpp
@@ -8,11 +8,31 @@ token lookahead;
 token proximo_token(void);
 void erro(void);
 void reconhecer(token);
+void expr(void);
+void digito(void);
+void resto(void);
 
 int main(void){
 	token t = proximo_token();
 	cout << t << endl;
 	return 0;
+}
+void resto(void){
+	if(lookahead == '+' or lookahead == '-'){
+		auto c = lookahead;
+		reconhecer(c);
+		digito();
+		cout.put(c);
+		resto();
+	}
+}
+void digito(void){
+	if(isdigit(lookahead)){
+		cout.put(lookahead);
+		reconhecer(lookahead);
+	}else{
+		erro();
+	}
 }
 token proximo_token(void)
 {
@@ -28,4 +48,8 @@ void reconhecer(token t){
 		lookahead = proximo_token();
 	else
 		erro();
+}
+void expr(void){
+	digito();
+	resto();
 }
