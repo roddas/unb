88480c665b02643ed098037b8a8936bec7c507c1
Rodolfo Cabral Neves <roddas360@gmail.com>
2023-05-01 16:18:04 -0300

    feat: Add the error routine and lookahead variable

--- compiladores/proximo_token.cpp
+++ compiladores/proximo_token.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 using token  = char;
 
+token lookahead;
+
 token proximo_token(void);
+void erro(void);
 
 int main(void){
 	token t = proximo_token();
@@ -14,4 +17,8 @@ token proximo_token(void)
 {
 	auto t = cin.get();
 	return (token)t;	
+}
+void erro(void){
+	cerr << "\nErro de sintaxe! lookahead = " << lookahead << '\n';
+	exit(-1);
 }
